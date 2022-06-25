d8ac30c949c462e676fb14e446ffa272d17b4ae7
Maksim <maksimus.tumannick@yandex.ru>
2022-06-25 13:33:48 +0300

    Task2.9

---
+++ Task2.9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int maxValue = 28;
+            int minValue = 1;
+            int number = random.Next(minValue, maxValue);
+            int minValueThreeDigitNumber = 99;
+            int maxValueThreeDigitNumber = 999;
+            List<int> amountOfNumber = new List<int>();
+
+            for (; number <= minValueThreeDigitNumber; number += number)
+            {
+                for (; number < maxValueThreeDigitNumber; number += number)
+                {
+                    amountOfNumber.Add(number);
+                }
+            }
+        }
+    }
+}
